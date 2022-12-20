5e451a33ed64f73ec33e1768c84e024c4e4749d0
Denis <den-apple@ya.ru>
2022-12-20 20:03:02 +0200

    удалить файлы

--- backend/middlewares/limiter.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 450,
-  message: 'Превышен лимит количества запросов к серверу',
-});
-
-module.exports = { limiter };
