2a558e3751d5094b3d4023837f517c0ef080d603
Denis <den-apple@ya.ru>
2022-12-22 20:16:09 +0200

    тестовая сборка

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-function ProtectedRoute({ loggedIn, children }) {
-  return loggedIn ? children : <Navigate to="sign-in" />;
-}
-
-export default ProtectedRoute;
